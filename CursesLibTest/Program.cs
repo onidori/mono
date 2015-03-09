92b5fe48c46cf9181999e94dcde3280aed8fa97d
onidori <onidori.hades@gmail.com>
2015-03-08 20:53:25 -0400

    Clean up project files

---
+++ CursesLibTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using MyLibrary.System;
+
+namespace CursesLibTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			PlatformID pid = MyCheckOS ();
+			string LibString;
+
+			switch (pid) {
+			case PlatformID.Unix:
+					LibString = "ncurses";
+				break;
+			case PlatformID.MacOSX:
+				LibString = "libncurses.dylib";
+				break;
+			default:
+				LibString = "unknown";
+				Console.WriteLine ("Unknown NCurses Library");
+				break;
+			}
+			Console.WriteLine (Enum.GetName (typeof(PlatformID), pid));
+			Console.WriteLine (LibString);
+		}
+	}
+}
